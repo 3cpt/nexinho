787d4c0f6dd0dd031e614827f5c4aa1ee85ed1a2
Andre Correia <andrecorreia@pm.me>
2021-04-08 23:46:45 +0100

    add read to start

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,11 @@
 git pull
 dotnet build -c Release
 
-cp -f ../configs/appsettings.json src/Nexinho/bin/Release/net5.0/
+#cp -f ../configs/appsettings.json src/Nexinho/bin/Release/net5.0/
 
-dotnet src/Nexinho/bin/Release/net5.0/Nexinho.dll
+cd src/Nexinho/bin/Release/net5.0/
+
+read -p "PRESS ENTER TO START"
+
+dotnet Nexinho.dll
 
