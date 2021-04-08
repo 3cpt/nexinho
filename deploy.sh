c78044c5c5cb8cd118b4722e1b032efd77606409
Andre Correia <andrecorreia@pm.me>
2021-04-08 23:42:45 +0100

    fix copy settings

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 git pull
 dotnet build -c Release
 
-cp -f ../configs/appsettings.json src/Nexinho
+cp -f ../configs/appsettings.json src/Nexinho/bin/Release/net5.0/
 
 dotnet src/Nexinho/bin/Release/net5.0/Nexinho.dll
 
